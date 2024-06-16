399bf65a6c0cfa5d93ab06c4a9169efaddcc7201
MK <muchunumike@gmail.com>
2024-06-16 15:46:13 +0300

    f(session_management): Init navigation component

---
+++ src/components/Nav.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface NavProps {
+
+}
+
+const Nav: React.FC <NavProps> = props => {
+    return (
+        <nav className="">
+
+        </nav>
+    )
+}
+
+export default Nav;
