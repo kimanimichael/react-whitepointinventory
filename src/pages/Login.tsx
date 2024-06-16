19636d30bc580fdd4e7f63bc5aab3d9fbab36a58
MK <muchunumike@gmail.com>
2024-06-16 15:46:42 +0300

    f(session_management): Init login component

---
+++ src/pages/Login.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface LoginProps {
+
+}
+
+const Login: React.FC <LoginProps> = props => {
+    return (
+        <div className="">
+
+        </div>
+    )
+}
+
+export default Login;
