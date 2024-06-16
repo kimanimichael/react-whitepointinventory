06c16797e41cb32e1016c0d0ef30e4a573ffb2b4
MK <muchunumike@gmail.com>
2024-06-16 15:46:28 +0300

    f(session_management): Init home component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface HomeProps {
+
+}
+
+const Home: React.FC <HomeProps> = props => {
+    return (
+        <div className="" >
+
+        </div>
+    )
+}
+
+export default Home;
