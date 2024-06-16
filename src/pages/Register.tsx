15358daf27c9d4e73a8a3f48d6c9fd189e9e52e2
MK <muchunumike@gmail.com>
2024-06-16 15:46:57 +0300

    f(session_management): Init register component

---
+++ src/pages/Register.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface RegisterProps {
+
+}
+
+const Register: React.FC <RegisterProps> =props => {
+    return (
+        <div className="">
+
+        </div>
+    )
+}
+
+export default Register;
