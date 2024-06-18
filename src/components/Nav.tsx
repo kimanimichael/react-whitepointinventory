ba8d8464cc1e911a7784300bccda46db186c0e8e
MK <muchunumike@gmail.com>
2024-06-18 13:01:46 +0300

    f(session_management): Init nav bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,15 +1,56 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 interface NavProps {
-
+    // name: string
 }
 
 const Nav: React.FC <NavProps> = props => {
-    return (
-        <nav className="">
+    // const logout = async () => {
+    //     await fetch('http://localhost:8000/api/logout', {
+    //         method: 'POST',
+    //         headers: {'Content-Type': 'application/json'},
+    //         credentials: 'include'
+    //     })
+    // }
+    // let menu;
 
+    // if (props.name === undefined) {
+    //     menu = (
+    //         <ul className="navbar-nav me-auto mb-2 mb-md-0">
+    //             <li className="nav-item">
+    //                 <Link to="/login" className="nav-link active">Login</Link>
+    //             </li>
+    //             <li className="nav-item">
+    //                 <Link to="/register" className="nav-link active">Register</Link>
+    //             </li>
+    //         </ul>
+    //     )
+    // }
+    // else {
+    //     menu = (
+    //         <ul className="navbar-nav me-auto mb-2 mb-md-0">
+    //             <li className="nav-item">
+    //                 <Link to="/login" className="nav-link active" onClick={logout}>Logout</Link>
+    //             </li>
+    //         </ul>
+    //     )
+    // }
+    return (
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+            <div className="container-fluid">
+                <Link to="/" className="navbar-brand">Home</Link>
+                <Link to="/payments" className="navbar-brand">Payments</Link>
+                <Link to="/purchases" className="navbar-brand">Purchases</Link>
+                <Link to="/farmers" className="navbar-brand">Farmers</Link>
+                <Link to="/users" className="navbar-brand">Users</Link>
+                <div>
+
+                    <Link to="/logout" className="navbar-brand">Logout</Link>
+                </div>
+            </div>
         </nav>
-    )
-}
+    );
+};
 
 export default Nav;
