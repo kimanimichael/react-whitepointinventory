c7504f76c149084dcb037827c0bf4808d2e10caf
MK <muchunumike@gmail.com>
2024-06-20 15:56:33 +0300

    f(session_management): Implement logout

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,52 +3,50 @@ import {Link} from "react-router-dom";
 
 interface NavProps {
     name: string
+    setName: (name: string) => void
 }
 
 const Nav: React.FC <NavProps> = props => {
-    // const logout = async () => {
-    //     await fetch('http://localhost:8000/api/logout', {
-    //         method: 'POST',
-    //         headers: {'Content-Type': 'application/json'},
-    //         credentials: 'include'
-    //     })
-    // }
-    // let menu;
+    const logout = async () => {
+        await fetch('http://localhost:8080/v1/logout', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            credentials: 'include'
+        })
+        props.setName('')
+        console.log('Logout button hit')
+    }
 
-    // if (props.name === undefined) {
-    //     menu = (
-    //         <ul className="navbar-nav me-auto mb-2 mb-md-0">
-    //             <li className="nav-item">
-    //                 <Link to="/login" className="nav-link active">Login</Link>
-    //             </li>
-    //             <li className="nav-item">
-    //                 <Link to="/register" className="nav-link active">Register</Link>
-    //             </li>
-    //         </ul>
-    //     )
-    // }
-    // else {
-    //     menu = (
-    //         <ul className="navbar-nav me-auto mb-2 mb-md-0">
-    //             <li className="nav-item">
-    //                 <Link to="/login" className="nav-link active" onClick={logout}>Logout</Link>
-    //             </li>
-    //         </ul>
-    //     )
-    // }
-    return (
-        <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+    let menu;
+    if (props.name === undefined) {
+        menu = (
             <div className="container-fluid">
                 <Link to="/" className="navbar-brand">Home</Link>
-                <Link to="/payments" className="navbar-brand">Payments</Link>
-                <Link to="/purchases" className="navbar-brand">Purchases</Link>
-                <Link to="/farmers" className="navbar-brand">Farmers</Link>
-                <Link to="/users" className="navbar-brand">Users</Link>
                 <div>
                     <Link to="/login" className="navbar-brand">Login</Link>
                     <Link to="/register" className="navbar-brand">SignUp</Link>
                 </div>
             </div>
+        )
+    } else {
+        menu = (
+            <div className="container-fluid">
+                <Link to="/" className="navbar-brand">Home</Link>
+                <Link to="/payments" className="navbar-brand">Payments</Link>
+                <Link to="/purchases" className="navbar-brand">Purchases</Link>
+                <Link to="/farmers" className="navbar-brand">Farmers</Link>
+
+                <div>
+                    <Link to="/users" className="navbar-brand">Users</Link>
+                    <Link to="/login" className="navbar-brand" onClick={logout}>Logout</Link>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+            {menu}
         </nav>
     );
 };
